c2eac4f16570354eb4d06f512f9596e8192fba6e
sorin800 <sorintruica@gmail.com>
2022-06-26 22:51:23 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,36 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
 import {Component} from "react";
 
 class App extends Component {
 
     constructor() {
+        // constructor runs first
+        console.log("1")
         super();
         this.state = {
-            name: { firstName: 'Sorin', lastName: 'Zhang'},
-            company: 'ZTM'
+            monsters: []
         }
+        console.log('constructor')
+    }
+
+    //the first time react renders a component it calls this method
+    //it's called only once when the component is first rendered on the page
+    componentDidMount() {
+        console.log('componentDidMount')
+        //this runs third after render is called
+        //when we fetch is this will give us a promise which is asynchronously
+        // if it succeds we can call then
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json()) //whatever is returned from here will be passed further
+            .then((users) => this.setState(() => {
+                return {monsters: users}
+            }, () => {console.log(this.state)}))
     }
 
 
-    render(){
-    return (
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Hi {this.state.name.firstName} {this.state.name.lastName}, I work at {this.state.company}
-            </p>
-            <button onClick={() => {
-                this.setState({name: 'Andrei'})
-                console.log(this.state)
-            }}>
-                Change Name
-            </button>
-          </header>
-        </div>
-    );
-  }
+    render() {
+        console.log("render")
+        //render runs second after constructor
+        return (
+            <div className="App">
+                {
+                    this.state.monsters.map((monster) => {
+                        return <div key={monster.name}>
+                            <h1>{monster.name}</h1>
+                        </div>;
+                    })
+                }
+            </div>
+        );
+    }
 
 }
 
