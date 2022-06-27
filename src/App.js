149afbba125ac385bb679acb164fb587f3ed14e6
Sorin Truica <sorin.truica@n26-external.com>
2022-06-27 18:51:33 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -31,25 +31,28 @@ class App extends Component {
     }
 
 
+    onSearchChange = (event) => {
+        const searchField = event.target.value.toLocaleLowerCase();
+
+        this.setState(() => {
+            return {searchField}
+        })
+    }
+
     render() {
         console.log("render")
         //render runs second after constructor
-        const filteredMonsters = this.state.monsters.filter((monster) => {
-            return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+
+        const {monsters, searchField} = this.state;
+        const {onSearchChange} = this;
+
+        const filteredMonsters = monsters.filter((monster) => {
+            return monster.name.toLocaleLowerCase().includes(searchField);
         })
 
         return (
             <div className="App">
-                <input className='search-box' type='search' placeholder='search monsters' onChange={(event) => {
-                    // this.setState({monsters: this.state.monsters.filter( monster => { return monster.name.includes(event.target.value)})})
-                    console.log(event.target.value)
-                    const searchField = event.target.value.toLocaleLowerCase();
-
-
-                    this.setState(() => {
-                        return {searchField}
-                    })
-                }}/>
+                <input className='search-box' type='search' placeholder='search monsters' onChange={onSearchChange}/>
                 {
                     filteredMonsters.map((monster) => {
                         return <div key={monster.name}>
